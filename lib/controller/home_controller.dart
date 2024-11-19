99640d84d9d382c1ed72015dd07494cdd076d8de
liza0012 <liza080btcse21@igdtuw.ac.in>
2024-11-19 22:25:17 +0530

    added backend+ dynamic feature

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:client_footwear/models/product/product.dart';
+import 'package:client_footwear/models/product_category/product_category.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,8 +7,12 @@ import 'package:get/get.dart';
 class HomeController extends GetxController{
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   late CollectionReference productCollection;
-  
+  late CollectionReference categoryCollection;
+
   List<Product> products = [];
+  List<Product> productShowInUi = [];
+  
+  List<ProductCategory> productCategories = [];
 
   get categories => null;
 
@@ -18,7 +23,11 @@ class HomeController extends GetxController{
     // TODO: implement onInit
 
     productCollection = firestore.collection('products');
+    categoryCollection = firestore.collection('category');
     await fetchProducts();
+    super.onInit();
+
+    await fetchCategory();
     super.onInit();
   }
 
@@ -31,7 +40,23 @@ class HomeController extends GetxController{
       
       products.clear();
       products.assignAll(retrievedProducts);
+      productShowInUi.assignAll(products);
       Get.snackbar('Success', 'Product fetched successfully',colorText: Colors.green);
+    }catch (e) {
+      Get.snackbar('Error', e.toString(),colorText: Colors.red);
+    }finally {
+      update();
+    }
+  }
+  fetchCategory() async {
+    try {
+      QuerySnapshot categorySnapshot = await categoryCollection.get();
+      final List<ProductCategory> retrievedCategories = categorySnapshot.docs.map((doc) => 
+                  ProductCategory.fromJson(doc.data() as Map<String, dynamic>)).toList();
+      
+      productCategories.clear();
+      productCategories.assignAll(retrievedCategories);
+      Get.snackbar('Success', 'Category fetched successfully',colorText: Colors.green);
     }catch (e) {
       Get.snackbar('Error', e.toString(),colorText: Colors.red);
     }finally {
@@ -43,6 +68,28 @@ class HomeController extends GetxController{
 
   void sortProducts(String? selected) {}
 
+  filterByCategory(String category){
+    productShowInUi.clear();
+    productShowInUi=products.where((product)=> product.category == category).toList();
+    update();
+  }
+
+  filterByBrand(List<String> brands){
+    if(brands.isEmpty){
+      productShowInUi=products;
+    }else{
+      List<String> lowerCaseBrands= brands.map((brand)=> brand.toLowerCase()).toList();
+      productShowInUi=products.where((product) => lowerCaseBrands.contains(product.brand?.toLowerCase() ?? '')).toList();
+    }
+    update();
+  }
+
+  sortByPrice({required bool ascending}){
+    List<Product> sortedProducts= List<Product>.from(productShowInUi);
+    sortedProducts.sort((a, b) => ascending ? a.price!.compareTo(b.price!): b.price!.compareTo(a.price!));
+    productShowInUi=sortedProducts;
+    update();
+  }
 }
 
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,88 +8,105 @@ import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart'; // For GetStorage
 import 'package:client_footwear/pages/login_page.dart'; // Import your LoginPage class
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
-    return GetBuilder<HomeController>(builder: (ctrl){
-      return Scaffold(
-      appBar: AppBar(
-        title: Text('Footwear Store',
-         style: TextStyle(
-          fontWeight: FontWeight.bold,
-         ),
-         ),
-
-         actions: [
-          IconButton(onPressed: () {
-            GetStorage box=GetStorage();
-            box.erase();
-            Get.offAll(LoginPage());
-
-          },
-          icon: Icon(Icons.logout))
-         ],
-      ),
-
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index){
-                return Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: Chip(label: Text('category')),
-                );
-            }),
-          ),
-
-          Row(
-            children: [
-              Flexible(
-                child: DropdownBtn(
-                  items: ['Rs: Low to High', 'Rs: High to Low'], 
-                  selectedItemText: 'Sort',
-                  onSelected: (selected) {},
-                ),
+    return GetBuilder<HomeController>(builder: (ctrl) {
+      return RefreshIndicator(
+        onRefresh: () async {
+          ctrl.fetchProducts();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Footwear Store',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
-              Flexible(child: MultiSelectDropDown(items: ['item1', 'item2', 'item3'],
-               onSelectionChanged: (selectedItems) {},
-               )),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    GetStorage box = GetStorage();
+                    box.erase();
+                    Get.offAll(LoginPage());
+                  },
+                  icon: Icon(Icons.logout))
             ],
           ),
+          body: Column(
+            children: [
+              SizedBox(
+                height: 50,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: ctrl.productCategories.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          ctrl.filterByCategory(
+                              ctrl.productCategories[index].name ?? 'Error');
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: Chip(
+                              label: Text(ctrl.productCategories[index].name ??
+                                  'Error')),
+                        ),
+                      );
+                    }),
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: DropdownBtn(
+                      items: ['Rs: Low to High', 'Rs: High to Low'],
+                      selectedItemText: 'Sort',
+                      onSelected: (selected) {
+                        ctrl.sortByPrice(ascending: selected == 'Rs: Low to High'? true: false);
+                      },
+                    ),
+                  ),
+                  Flexible(
+                      child: MultiSelectDropDown(
+                    items: ['Prada', 'Adidas', 'Nike', 'Skechers', 'Bata'],
+                    onSelectionChanged: (selectedItems) {
+                      print(selectedItems);
+                      ctrl.filterByBrand(selectedItems);
+                    },
+                  )),
+                ],
+              ),
+              Expanded(
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.8,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8),
+                    itemCount: ctrl.productShowInUi.length,
+                    itemBuilder: (context, index) {
+                      return ProductCard(
+                        name: ctrl.productShowInUi[index].name ?? 'No name',
+                        imageUrl: ctrl.productShowInUi[index].image ?? 'url',
+                        price: ctrl.productShowInUi[index].price ?? 00,
+                        offerTag: '20% off',
+                        onTap: () {
+                          Get.to(ProductDescriptionPage(), arguments: {'data': ctrl.productShowInUi[index]});
 
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 0.8,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8
-            ), 
-            itemCount: ctrl.products.length,
-            itemBuilder: (context, index) {
-              return ProductCard(
-                name: ctrl.products[index].name ?? 'No name', 
-                imageUrl: ctrl.products[index].image??'url',
-                price: ctrl.products[index].price?? 00, 
-                offerTag: '20% off', 
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ProductDescriptionPage()),
-                    );
-                },
-                );
-            }),
-          )
-
-        ],
-      ),
-    );
+                        },
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      );
     });
   }
 }
